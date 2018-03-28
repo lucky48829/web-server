b365930b668a4c49788e2d262c89d76d0741e3f2
emilence <lucky48829@gmail.com>
2018-03-28 17:58:03 +0530

    Update port for heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express=require('express');
 var app=express();
-var PORT=3000;
+var PORT=process.env.PORT|| 3000;
 var middleware=require('./middleware.js');
+
 app.use(middleware.logger);
 
 app.get("/about",middleware.requireAuthentication,function(req,res){
@@ -9,6 +10,7 @@ app.get("/about",middleware.requireAuthentication,function(req,res){
 });
 
 app.use(express.static(__dirname+'/public'));
+
 app.listen(PORT,function(){
 	console.log('Express server started on port' +PORT+ '!');
 });
